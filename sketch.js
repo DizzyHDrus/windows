8cc57a1724f7227ef2c6d782eef52e6c5a3ec2c1
TheDramaMan <antonpi2011@ya.ru>
2015-09-26 23:20:55 +0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var names = ["Text", "Getup1NEW", "p5.js", "TheDramaMan"];
+function setup() {
+	createCanvas(screen.width-20, screen.height-155);
+	fill(255);
+	background(256);		
+	stroke(2, 2, 230);
+}
+
+function draw() {
+	if(mouseIsPressed) {
+		fill(240);
+		rect(mouseX, mouseY, 100, 70);
+		fill(2, 2, 230);
+		rect(mouseX, mouseY, 100, 12);
+		stroke(2, 2, 230);
+		fill(255, 2, 2);
+		rect(mouseX+90, mouseY+4, 4, 4);
+		fill(248, 243, 132);
+		rect(mouseX+85, mouseY+4, 4, 4);
+		fill(2, 255, 2);
+		rect(mouseX+80, mouseY+4, 4, 4);
+		stroke(255);
+		fill(255);
+		
+		rect(mouseX+3, mouseY+4, 4, 4);
+		
+		fill(1);
+   text(names[(Math.floor(Math.random()*3))], mouseX+4, mouseY+28);
+
+		
+
+	} else {
+//TODO: implement idling function
+	}
+}
