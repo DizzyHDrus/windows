0617ecd52a13dfbdb8c52b27c9a96a382a799f29
TheDramaMan <antonpi2011@ya.ru>
2015-09-27 20:04:39 +0300

    Small fixes and icon!

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var names = ["Text", "Getup1NEW", "p5.js", "TheDramaMan"];
+var names = ["Text", "Getup1NEW", "p5.js", "TheDramaMan", "<--- This message will never shown!"];
 function spawn(){
 fill(240);
 		rect(mouseX, mouseY, 100, 70);
@@ -15,6 +15,9 @@ fill(240);
 		fill(255);
 		
 		rect(mouseX+3, mouseY+4, 4, 4);
+		
+		fill(250, 100, 20);
+		ellipse(mouseX+4, mouseY+42, 2, 2);
 		
 		fill(1);
    text(names[(Math.floor(Math.random()*3))], mouseX+4, mouseY+28);
